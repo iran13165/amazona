1a1f08dc29c9149c06f53b2fa752234bb6f50c3b
iran13165 <iran.choudhury@gmail.com>
2022-08-26 02:07:50 +0530

    video-3-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+      <a href="/">amazona</a>
+      </header>
+      <main>
+        list product
+      </main>
+    </div>
+  );
+}
+
+export default App;
