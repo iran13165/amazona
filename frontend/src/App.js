3df56ec7af27aff5837e60949463a3d42386f2e2
iran13165 <iran.choudhury@gmail.com>
2022-08-30 20:44:24 +0530

    video-33-publish-t0-heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,111 @@
 import './App.css';
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
+import NavBar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useContext } from 'react';
+import { Store } from './Store';
+import CartScreen from './Screens/CartScreen';
+import SigninScreen from './Screens/SigninScreen';
+import ShippingAddressScreen from './Screens/ShippingAddressScreen';
+import SignupScreen from './Screens/SignupScreen';
+import PaymentMethodScreen from './Screens/PaymentMethodScreen';
+import PlaceOrderScreen from './Screens/PlaceOrderScreen';
+import OrderScreen from './Screens/OrderScreen';
+import OrderHistoryScreen from './Screens/OrderHistoryScreen';
+import ProfileScreen from './Screens/ProfileScreen';
+
 function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+  const signOutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
+    window.location.href = '/signin';
+  };
   return (
     <BrowserRouter>
-    <div>
-      <header>
-        <Link to="/">amazona</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path='/product/:slug' element={<ProductScreen/>}/>
-          <Route path='/' element={<HomeScreen/>}/>
-        </Routes>
+      <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1} />
+        <header>
+          <NavBar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <LinkContainer to="/">
+                <NavBar.Brand>amazona</NavBar.Brand>
+              </LinkContainer>
+              <NavBar.Toggle aria-controls='basic-navbar-nav'/>
+              <NavBar.Collapse id='basic-navbar-nav'>
+              <Nav className="me-auto w-100 justify-content-end" >
+                <Link to={'/cart'} className="nav-link">
+                  Cart{' '}
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to={'/profile'}>
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to={'/orderhistory'}>
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to={'#signout'}
+                      onClick={signOutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to={'/signin'}>
+                    Sign In
+                  </Link>
+                )}
+              </Nav>
+              </NavBar.Collapse>
+            </Container>
+          </NavBar>
+        </header>
 
-      </main>
-    </div>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/order/:id" element={<OrderScreen />} />
+              <Route path="/orderhistory" element={<OrderHistoryScreen />} />
+
+
+              <Route path="/shipping" element={<ShippingAddressScreen />} />
+              <Route path="/payment" element={<PaymentMethodScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved.</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useReducer } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -17,7 +21,7 @@ const reducer = (state, action) => {
 
 const HomeScreen = () => {
   //const [products, setProducts] = useState([]);
-  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+  const [{ products, loading, error }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: '',
@@ -28,6 +32,7 @@ const HomeScreen = () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
         const result = await axios.get('/api/products');
+        console.log(result.data);
         dispatch({
           type: 'FETCH_SUCCESS',
           payload: result.data,
@@ -44,32 +49,31 @@ const HomeScreen = () => {
       //setProducts(result.data);
     };
     fetchData();
-  }, []);
+  }, [error.message]);
   return (
     <div>
+      <Helmet>
+        <title>Amazona</title>
+      </Helmet>
       <h1>Featured Product</h1>
       <div className="products">
         {loading ? (
-          <div>Loading</div>
+          <LoadingBox/>
         ) : error ? (
-          <div>error</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add To Cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {console.log(products)}
+            {
+              
+            products.products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}/>
+              </Col>
+            ))
+            
+            }
+          </Row>
         )}
       </div>
     </div>
